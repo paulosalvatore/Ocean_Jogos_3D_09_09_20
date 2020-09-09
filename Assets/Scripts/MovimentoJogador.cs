a8083ce78731d04ba2cb8ab30dd17a13bc94f86d
Paulo Salvatore <paulo@salvatore.tech>
2020-09-09 20:07:38 -0300

    Rotação do jogador e diferentes câmeras implementadas

--- Assets/Scripts/MovimentoJogador.cs
+++ Assets/Scripts/MovimentoJogador.cs
@@ -8,11 +8,16 @@ public class MovimentoJogador : MonoBehaviour
 
     public float velocidade = 1f;
 
+    public float velocidadeRotacao = 30f;
+
     // Update is called once per frame
     void Update()
     {
         var v = Input.GetAxis("Vertical") * velocidade;
+        var h = Input.GetAxis("Horizontal") * velocidadeRotacao;
 
-        rb.velocity = Vector3.forward * v;
+        rb.velocity = transform.forward * v;
+
+        transform.Rotate(Vector3.up * h * Time.deltaTime);
     }
 }
