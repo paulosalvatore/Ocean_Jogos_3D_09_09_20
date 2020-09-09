02581469f06583f6cdb46b02c2a0a3e24afdb415
Paulo Salvatore <paulo@salvatore.tech>
2020-09-09 20:51:27 -0300

    Terrain criado, com alguns objetos, movimentação corrigida e thirdPersonController adicionado

--- Assets/Scripts/MovimentoJogador.cs
+++ Assets/Scripts/MovimentoJogador.cs
@@ -18,12 +18,13 @@ public class MovimentoJogador : MonoBehaviour
         var v = Input.GetAxis("Vertical") * velocidade;
         var h = Input.GetAxis("Horizontal") * velocidadeRotacao;
 
-        rb.velocity = transform.forward * v;
+        var movimento = transform.forward * v;
+        rb.velocity = new Vector3(movimento.x, rb.velocity.y, movimento.z);
 
-        if (rb.velocity.magnitude > 0)
-        {
+        //if (rb.velocity.magnitude > 0)
+        //{
             transform.Rotate(Vector3.up * h * Time.deltaTime);
-        }
+        //}
 
         anim.SetFloat("velocidade", rb.velocity.magnitude);
     }
